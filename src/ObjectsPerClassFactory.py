b453ecbc3eb0a30205b756c3e18d0dd7d79015c5
Pierre Monnin <pier.monn@gmail.com>
2017-08-24 18:53:02 +0200

    Reorganize ObjectsPerClassFactory pre-multithreading

--- src/ObjectsPerClassFactory.py
+++ src/ObjectsPerClassFactory.py
@@ -8,6 +8,43 @@ class ObjectsPerClassFactory:
     def __init__(self, server_manager):
         self._server_manager = server_manager
 
+    def __get_objects_number_for_class(self, ontology_class, configuration_parameters):
+        ret_val = [ontology_class, '', '']
+        done = False
+
+        while not done:
+            try:
+                # Number of objects typed (asserted)
+                asserted_objects_query = configuration_parameters["objects-per-class-prefix"] \
+                    + " select count(distinct ?object) as ?count where { " \
+                    + "?object " + configuration_parameters["type-predicate"] + " <" \
+                    + ontology_class + "> }"
+                asserted_objects_count_json = self._server_manager.query_server(asserted_objects_query)
+
+                # Number of objects typed (asserted or inferred from ontology class hierarchy)
+                inferred_asserted_objects_query = configuration_parameters["objects-per-class-prefix"] \
+                    + " select count(distinct ?object) as ?count where { " \
+                    + "?object " + configuration_parameters["type-predicate"] + "/" \
+                    + configuration_parameters["parents-relationship"] + "* <" \
+                    + ontology_class + "> }"
+                inferred_asserted_objects_json = self._server_manager.query_server(inferred_asserted_objects_query)
+
+                ret_val = [
+                                ontology_class,
+                                asserted_objects_count_json["results"]["bindings"][0]["count"]["value"],
+                                inferred_asserted_objects_json["results"]["bindings"][0]["count"]["value"]
+                ]
+                done = True
+
+            except urllib.error.HTTPError as e:
+                print("\rHTTP error " + str(e.getcode()) + " while querying objects of " + ontology_class)
+                if e.getcode() == 404:
+                    print("New try")
+                else:
+                    done = True
+
+        return ret_val
+
     def get_objects_number_per_class(self, ontology, configuration_parameters):
         ret_val = []
 
@@ -16,38 +53,7 @@ class ObjectsPerClassFactory:
                              % (i * 100.0 / ontology.get_classes_number()))
             sys.stdout.flush()
 
-            done = False
-            while not done:
-                try:
-                    # Number of objects typed (asserted)
-                    asserted_objects_query = configuration_parameters["objects-per-class-prefix"] \
-                                             + " select count(distinct ?object) as ?count where { " \
-                                             + "?object " + configuration_parameters["type-predicate"] + " <" \
-                                             + ontology_class + "> }"
-                    asserted_objects_count_json = self._server_manager.query_server(asserted_objects_query)
-
-                    # Number of objects typed (asserted or inferred from ontology class hierarchy)
-                    inferred_asserted_objects_query = configuration_parameters["objects-per-class-prefix"] \
-                        + " select count(distinct ?object) as ?count where { " \
-                        + "?object " + configuration_parameters["type-predicate"] + "/" \
-                        + configuration_parameters["parents-relationship"] + "* <" \
-                        + ontology_class + "> }"
-                    inferred_asserted_objects_json = self._server_manager.query_server(inferred_asserted_objects_query)
-
-                    ret_val.append([ontology_class,
-                                    asserted_objects_count_json["results"]["bindings"][0]["count"]["value"],
-                                    inferred_asserted_objects_json["results"]["bindings"][0]["count"]["value"]])
-                    done = True
-
-                except urllib.error.HTTPError as e:
-                    print("\rHTTP error " + str(e.getcode()) + " while querying objects of " + ontology_class)
-                    if e.getcode() == 404:
-                        print("New try")
-                    else:
-                        ret_val.append([ontology_class, '', ''])
-                        done = True
-
-            i += 1
+            ret_val.append(self.__get_objects_number_for_class(ontology_class, configuration_parameters))
 
         print("\rQuerying number of objects per ontology class 100 %\t\t")
 
